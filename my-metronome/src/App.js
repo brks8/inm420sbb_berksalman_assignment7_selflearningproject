652e59fcdba960e4350b6e438c42dd4938d5e253
brks8 <fberksalman@gmail.com>
2025-08-04 19:57:54 -0400

    Setup React

---
+++ my-metronome/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={{ padding: '20px', textAlign: 'center' }}>
+      <h1>My Metronome</h1>
+      <p>BPM: 120</p>
+      <button>Start</button>
+    </div>
+  );
+}
+
+export default App;
